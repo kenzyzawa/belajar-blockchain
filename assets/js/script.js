f58d6f428654fbff0fc7d4aef656d0c852876438
FaItuDev <kenzyzawa@gmail.com>
2026-02-15 21:53:02 +0700

    add DOM manipulation feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 const button = document.getElementById("btn");
+const title = document.getElementById("title");
+const desc = document.getElementById("desc");
 
 button.addEventListener("click", function() {
-    alert("Welcome to my blockchain jurney");
-})
+    title.textContent = "Welcome to Web3 🔥";
+    desc.textContent = "I am learning step by step to become a blockchain developer.";
+    title.style.color = "#ab21ce";
+});
+console.log("Script jalan")
